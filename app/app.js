46cff172f7c6d97fdba1cbc06bfc25103752564e
Sachin Kelkar <sachinkel19@gmail.com>
2015-12-12 23:16:22 +0530

    Add login and register

--- app/app.js
+++ app/app.js
@@ -22,13 +22,33 @@ angular
       })
       .state('login', {
         url: '/login',
-        templateUrl: 'auth/login.html'
+        templateUrl: 'auth/login.html',
+        controller: 'AuthCtrl as authCtrl',
+        resolve: {
+          requireNoAuth: function($state, Auth) {
+            return Auth.$requireAuth().then(function(auth) {
+              $state.go('home');
+            }, function(error) {
+              return;
+            });
+          }
+        }
       })
       .state('register', {
         url: '/register',
-        templateUrl: 'auth/register.html'
+        templateUrl: 'auth/register.html',
+        controller: 'AuthCtrl as authCtrl',
+        resolve: {
+          requireNoAuth: function($state, Auth) {
+            return Auth.$requireAuth().then(function(auth) {
+              $state.go('home');
+            }, function(error) {
+              return;
+            });
+          }
+        }
       });
 
     $urlRouterProvider.otherwise('/');
   })
-  .constant('FirebaseUrl', 'https://blazing-torch-2223.firebaseio.com/');
+  .constant('FirebaseUrl', 'https://s4-fireslack.firebaseio.com/');
